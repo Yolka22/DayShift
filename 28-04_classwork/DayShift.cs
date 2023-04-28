25d9d5fe832a62fe5e4a220f06deef157bfdefb4
Егор Корсун <egorkorsun7@gmail.com>
2023-04-28 18:01:08 +0200

    Добавьте файлы проекта.

---
+++ 28-04_classwork/DayShift.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace _28_04_classwork
+{
+    internal sealed class DayShiftExctension: MarkupExtension
+    {
+
+        private readonly int _dayShift;
+
+        public DayShiftExctension(int dayShift)
+        {
+
+            _dayShift = dayShift;
+
+        }
+
+
+        public DayShiftExctension()
+        {
+            _dayShift = 0;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            string day_of_week = DateTime.Now.AddDays(_dayShift).DayOfWeek.ToString();
+            return day_of_week;
+        }
+    }
+}
